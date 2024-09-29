646e764d9606cc594d1ddf7d956cad5c00de3def
steve-chavez <stevechavezast@gmail.com>
2024-09-28 19:41:45 -0500

    set cares mem callbacks

--- src/worker.c
+++ src/worker.c
@@ -23,6 +23,8 @@
 #include <curl/curl.h>
 #include <curl/multi.h>
 
+#include <ares.h>
+
 #include <sys/epoll.h>
 #include <sys/timerfd.h>
 #include <unistd.h>
@@ -491,7 +493,10 @@ void pg_net_worker(Datum main_arg) {
 
   elog(LOG, "pg_net_worker started with pid %d and config of: pg_net.ttl=%s, pg_net.batch_size=%d, pg_net.database_name=%s", MyProcPid, guc_ttl, guc_batch_size, guc_database_name);
 
+  ares_library_init_mem(ARES_LIB_INIT_ALL, pg_net_curl_malloc, pg_net_curl_free, pg_net_curl_realloc);
+
   int curl_ret = curl_global_init_mem(CURL_GLOBAL_ALL, pg_net_curl_malloc, pg_net_curl_free, pg_net_curl_realloc, pstrdup, pg_net_curl_calloc);
+
   /*int curl_ret = curl_global_init(CURL_GLOBAL_ALL);*/
   if(curl_ret != CURLE_OK)
     ereport(ERROR, errmsg("curl_global_init() returned %s\n", curl_easy_strerror(curl_ret)));
@@ -601,31 +606,7 @@ void pg_net_worker(Datum main_arg) {
       if(curl_ret != CURLM_OK)
         ereport(ERROR, errmsg("curl_multi_cleanup: %s", curl_multi_strerror(curl_ret)));
 
-      curl_global_cleanup();
-
-      close(ws.epfd);
-      close(ws.timerfd);
-
-      ws.epfd = epoll_create1(0);
-      ws.timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
-
-      if (ws.epfd < 0) {
-        ereport(ERROR, errmsg("Failed to create epoll file descriptor"));
-      }
-
-      if (ws.timerfd < 0) {
-        ereport(ERROR, errmsg("Failed to create timerfd"));
-      }
-
-      timerfd_settime(ws.timerfd, 0, &(itimerspec){}, NULL);
-
-      epoll_ctl(ws.epfd, EPOLL_CTL_ADD, ws.timerfd, &(epoll_event){.events = EPOLLIN, .data.fd = ws.timerfd});
-
-      curl_ret = curl_global_init(CURL_GLOBAL_ALL);
-      if(curl_ret != CURLE_OK)
-        ereport(ERROR, errmsg("curl_global_init() returned %s\n", curl_easy_strerror(curl_ret)));
-
-      ws.curl_mhandle = curl_multi_init(),
+      ws.curl_mhandle = curl_multi_init();
       curl_multi_setopt(ws.curl_mhandle, CURLMOPT_SOCKETFUNCTION, multi_socket_cb);
       curl_multi_setopt(ws.curl_mhandle, CURLMOPT_SOCKETDATA, &ws);
       curl_multi_setopt(ws.curl_mhandle, CURLMOPT_TIMERFUNCTION, multi_timer_cb);
